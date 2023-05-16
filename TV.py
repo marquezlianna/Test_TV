262a5d45188ddf7802644cdb523f27334b90f0d7
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-05-16 20:58:43 +0800

    Turn on and off

--- TV.py
+++ TV.py
@@ -4,3 +4,15 @@ class TV:
         self.channel = 1
         self.volume_level = 1
         self.on = False
+
+# The current channel (1 to 120) of this TV
+# The current volume level (1 to 7) of this TV
+# Indicates whether this TV is on/off
+
+    # Turn on this TV
+    def turn_on(self):
+        self.on = True
+        
+    # Turn off this TV
+    def turn_off(self):
+        self.on = False
